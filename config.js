729bd3e883ed49bba2c0c617f3cd64db5494b32d
unknown <jim@blingtron.gislers.org>
2019-10-12 12:37:55 -0700

    updated to avoid import issues

--- config.js
+++ config.js
@@ -1,11 +1,43 @@
-#!/usr/bin/env node
-
-const ConfigHelper = require('../lib/ConfigHelper');
+#! /usr/bin/env node
 
 try {
-   ConfigHelper.getInstance().createEnvFile(process.argv[2]);
+   createEnvFile(process.argv[2]);
    process.exit(0);
 } catch (error) {
    console.error(JSON.stringify(error, null, 2));
    process.exit(1);
+}
+
+
+function createEnvFile(envName) {
+   process.env.NODE_ENV = envName;
+   const config = require('config');
+
+   const envFileContent = ['#! /usr/bin/env bash'];
+   envFileContent.push(...this.buildEnvConfig(config));
+   envFileContent.push(`export CloudFormationParams="${this.buildCloudFormationParameters(config)}"`);
+   envFileContent.push(`export TagParams="${this.buildTagParameters(config)}"`);
+
+   console.log(`Writing .env ... done!`);
+   const fs = require('fs');
+   return fs.writeFileSync('./.env', envFileContent.join('\n'));
+}
+
+function buildEnvConfig(config) {
+   const envConfig = config.get('Environment');
+   return Object.keys(envConfig).map(key => `export ${key}="${envConfig[key]}"`);
+}
+
+function buildCloudFormationParameters(config) {
+   const cfConfig = config.get('CloudFormation');
+   return Object.keys(cfConfig)
+      .map(key => `${key}=${cfConfig[key]}`)
+      .join(' ');
+}
+
+function buildTagParameters(config) {
+   const tagConfig = config.get('Tags');
+   return Object.keys(tagConfig)
+      .map(key => `${key}=${tagConfig[key]}`)
+      .join(' ');
 }
