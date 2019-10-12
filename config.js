56fe292778c5f038be0f532df1482d660687fa4f
unknown <jim@blingtron.gislers.org>
2019-10-12 14:02:03 -0700

    fixed 'this'

--- config.js
+++ config.js
@@ -1,43 +1,44 @@
 #! /usr/bin/env node
 
 try {
-   createEnvFile(process.argv[2]);
-   process.exit(0);
+  createEnvFile(process.argv[2]);
+  process.exit(0);
 } catch (error) {
-   console.error(JSON.stringify(error, null, 2));
-   process.exit(1);
+  console.error(JSON.stringify(error, null, 2));
+  process.exit(1);
 }
 
-
 function createEnvFile(envName) {
-   process.env.NODE_ENV = envName;
-   const config = require('config');
+  process.env.NODE_ENV = envName;
+  const config = require("config");
 
-   const envFileContent = ['#! /usr/bin/env bash'];
-   envFileContent.push(...this.buildEnvConfig(config));
-   envFileContent.push(`export CloudFormationParams="${this.buildCloudFormationParameters(config)}"`);
-   envFileContent.push(`export TagParams="${this.buildTagParameters(config)}"`);
+  const envFileContent = ["#! /usr/bin/env bash"];
+  envFileContent.push(...buildEnvConfig(config));
+  envFileContent.push(
+    `export CloudFormationParams="${buildCloudFormationParameters(config)}"`
+  );
+  envFileContent.push(`export TagParams="${buildTagParameters(config)}"`);
 
-   console.log(`Writing .env ... done!`);
-   const fs = require('fs');
-   return fs.writeFileSync('./.env', envFileContent.join('\n'));
+  console.log(`Writing .env ... done!`);
+  const fs = require("fs");
+  return fs.writeFileSync("./.env", envFileContent.join("\n"));
 }
 
 function buildEnvConfig(config) {
-   const envConfig = config.get('Environment');
-   return Object.keys(envConfig).map(key => `export ${key}="${envConfig[key]}"`);
+  const envConfig = config.get("Environment");
+  return Object.keys(envConfig).map(key => `export ${key}="${envConfig[key]}"`);
 }
 
 function buildCloudFormationParameters(config) {
-   const cfConfig = config.get('CloudFormation');
-   return Object.keys(cfConfig)
-      .map(key => `${key}=${cfConfig[key]}`)
-      .join(' ');
+  const cfConfig = config.get("CloudFormation");
+  return Object.keys(cfConfig)
+    .map(key => `${key}=${cfConfig[key]}`)
+    .join(" ");
 }
 
 function buildTagParameters(config) {
-   const tagConfig = config.get('Tags');
-   return Object.keys(tagConfig)
-      .map(key => `${key}=${tagConfig[key]}`)
-      .join(' ');
+  const tagConfig = config.get("Tags");
+  return Object.keys(tagConfig)
+    .map(key => `${key}=${tagConfig[key]}`)
+    .join(" ");
 }
