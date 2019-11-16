6f0e6b6ed61d18154ecc8e4b0c7be93e3c8e654e
unknown <jim@blingtron.gislers.org>
2019-11-16 09:16:09 -0800

    trying to use sam-cli

---
+++ sam_package.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+set -e
+
+if [ -z ${1} ];
+then
+    echo "Usage: sam_package.sh {templateName}";
+    exit 1;
+else
+    templateName=$1 && \
+    source .env && \
+    aws cloudformation package \
+        --template-file sam/${templateName}.yaml \
+        --output-template-file ${templateName}-out.yaml \
+        --s3-bucket ${DeploymentBucket} \
+        --s3-prefix ${DeploymentPrefix} \
+        --region ${Region} \
+        --profile ${Profile}
+fi
