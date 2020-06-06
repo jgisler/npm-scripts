b0eb4fd8c3b1f6e595c33ea02c726b698c144cca
unknown <jim@blingtron.gislers.org>
2020-06-06 08:57:11 -0700

    updated

--- zip_layer.sh
+++ zip_layer.sh
@@ -11,6 +11,5 @@ ZIP_FILE=dist.zip
 
 source .env && \
 npx rimraf ${ZIP_FILE} && \
-npm prune --production && npm dedupe && \
-zip -r dist.zip .env package.json src/ node_modules/ \
-    -x node_modules/.bin/ node_modules/.cache/
+npm --prefix nodejs prune --production && npm --prefix nodejs dedupe && \
+zip -r dist.zip nodejs/ -x node_modules/.bin/ node_modules/.cache/
