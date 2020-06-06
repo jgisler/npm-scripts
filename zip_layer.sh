42364dd1c5a423b1a06a3a19cd8242795a141a20
unknown <jim@blingtron.gislers.org>
2020-06-06 10:28:46 -0700

    updated

--- zip_layer.sh
+++ zip_layer.sh
@@ -6,10 +6,10 @@ then
     echo "Usage: zip_layer.sh {templateName}";
     exit 1;
 else
-
-ZIP_FILE=dist.zip
-
-source .env && \
-npx rimraf ${ZIP_FILE} && \
-npm --prefix nodejs prune --production && npm --prefix nodejs dedupe && \
-zip -r dist.zip nodejs/ -x node_modules/.bin/ node_modules/.cache/
+    ZIP_FILE=dist.zip
+    source .env && \
+    npx rimraf ${ZIP_FILE} && \
+    npm --prefix nodejs prune --production && \
+    npm --prefix nodejs dedupe && \
+    zip -r dist.zip nodejs/ -x node_modules/.bin/ node_modules/.cache/
+fi
